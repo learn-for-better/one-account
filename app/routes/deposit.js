11d342f899b8cbc44181ec73f207c31b6b7ce38d
gaoxiang <sece1024@gmail.com>
2024-05-13 15:09:40 +0800

    feat: add deposit api

--- app/routes/deposit.js
+++ app/routes/deposit.js
@@ -3,25 +3,28 @@ const router = express.Router();
 const Deposit = require('../models/Deposit');
 
 router.post('/', async (req, res) => {
-    const { deposits } = req.body;
+    const deposits = req.body;
     const date = new Date();
     const now = date.toISOString();
 
     if (deposits && deposits.length > 0) {
-        const insertedGraph = await Deposit.transaction(async trx => {
-            await Deposit.query(trx).insertGraph(deposits.map(deposit => {
-                return {
-                    date: now,
-                    amount: deposit.amount,
-                    description: deposit.description
-                }
-            }));
+        try {
+            const insertedGraph = await Deposit.transaction(async trx => {
+                await Deposit.query(trx).insertGraph(deposits.map(deposit => {
+                    return {
+                        date: now,
+                        amount: deposit.amount,
+                        description: deposit.description
+                    }
+                }));
+            });
 
-        });
-
-        res.json(insertedGraph);
+            res.status(201).json(insertedGraph);
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
-
 });
 
 router.get('/', async (req, res) => {
@@ -29,6 +32,19 @@ router.get('/', async (req, res) => {
         const deposits = await Deposit.query();
 
         res.json(deposits);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deposit = await Deposit.query().deleteById(id);
+
+        res.json(deposit);
     } catch (e) {
         console.log(e);
         res.status(500).json({ error: 'Internal Server Error' });
